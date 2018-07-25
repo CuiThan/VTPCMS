ed770b30d7ca2a308396fc8bccb3743f987722c7
Kien Nguyen Duc <kiennguyenduc95@gmail.com>
2018-07-25 10:20:21 +0700

    add cronjob to update fb user info

--- dao/facebook-user.js
+++ dao/facebook-user.js
@@ -1,21 +1,23 @@
 var mongoose = require('mongoose');
 var FacebookUserSchema = new mongoose.Schema({
    user_id: String,
-   name: String,
-   first_name : String,
-   last_name : String,
-   middle_name : String,
-   address: String,
-   hometown: String,
-   education: String,
-   gender: String,
-   location: String,
-   relationship_status: String,
-   age_range: [],
-   birthday: Date,
-   about: String,
-   email: String,
+   // name: String,
+   // first_name : String,
+   // last_name : String,
+   // middle_name : String,
+   // address: String,
+   // hometown: String,
+   // education: String,
+   // gender: String,
+   // location: String,
+   // relationship_status: String,
+   // age_range: [],
+   // birthday: Date,
+   // about: String,
+   // email: String,
    phone: String,
+   status: { type: Number, default: 0 },
+   info: []
 });
 
 mongoose.model('FacebookUser', FacebookUserSchema);
--- vtp_cms_api/UserController.js
+++ vtp_cms_api/UserController.js
@@ -7,6 +7,7 @@ var FacebookUser = require('../dao/facebook-user');
 var Employee = require('../dao/employee');
 var fs = require('fs');
 var axios = require('axios');
+var schedule = require('node-schedule')
 var path = require('path');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
@@ -23,11 +24,11 @@ router.get('/list-facebook-user/:page', function (req, res) {
     var page = req.params.page || 1;
 
    FacebookUser.find({}).skip((perPage * page) - perPage).limit(perPage)
-   .exec(function(err, employeeList) {
+   .exec(function(err, userList) {
       FacebookUser.count().exec(function(err, count) {
           if (err) return next(err)
           res.render('list-facebook-user', {
-              employeeList: employeeList,
+              userList: userList,
               maxPage: 13,
               current: page,
               pages: Math.ceil(count / perPage),
@@ -37,58 +38,92 @@ router.get('/list-facebook-user/:page', function (req, res) {
           })
       })
    })
-})
+});
+
+function asyncUpdate(i, array) {
+   if (i < array.length) {
+      var row = array[i];
+      axios.get('https://graph.facebook.com/' + row.user_id +
+         '?fields=birthday,age_range,about,address,education,email,first_name,gender,hometown,link,last_name,location,name,middle_name,relationship_status'
+         + '&access_token=283111455508809|l0GhSkkg0AHZkDEPimP50eBhz14'
+      )
+      .then( data => {
+         var user_info = data.data;
+         if(user_info.error) {
+            console.log(i + '- can not update');
+         } else {
+            FacebookUser.findByIdAndUpdate( row._id, {
+               status: 1,
+               info: user_info
+            }, function (err, obj) {
+               console.log('update ' + i);
+               asyncUpdate(i+1, array);
+            });
+         }
+      })
+      .catch( err => {
+         console.log(i + ' - catch error');
+         // console.log(err);
+         asyncUpdate(i+1, array);
+      })
+
+   } else {
+      console.log('finish update');
+   }
+};
 
 function asyncInsert(i, array) {
-      if (i < array.length) {
-         var line = array[i].split('|');
-         FacebookUser.create({
-                  user_id: line[0],
-                  phone: line[1]
-               }, function (err, obj) {
-                  console.log(i);
-                  asyncInsert(i+1, array);
-               });
-         // axios.get('https://graph.facebook.com/' + line[0] +
-         //    '?fields=birthday,age_range,about,address,education,email,first_name,gender,hometown,link,last_name,location,name,middle_name,relationship_status'
-         //    + '&access_token=283111455508809|l0GhSkkg0AHZkDEPimP50eBhz14'
-         // )
-         // .then( data => {
-         //    var user_info = data.data;
-         //    if(user_info.error) {
-         //       console.log(i + '- not exists');
-         //    } else {
-         //       FacebookUser.create({
-         //          user_id: line[0],
-         //          name: verify.checkUndefined(user_info.name),
-         //          first_name : verify.checkUndefined(user_info.first_name),
-         //          last_name : verify.checkUndefined(user_info.last_name),
-         //          middle_name : verify.checkUndefined(user_info.middle_name),
-         //          address: verify.checkUndefined(user_info.address),
-         //          hometown: verify.checkUndefined(user_info.hometown),
-         //          education: verify.checkUndefined(user_info.education),
-         //          gender: verify.checkUndefined(user_info.gender),
-         //          location: verify.checkUndefined(user_info.location),
-         //          relationship_status: verify.checkUndefined(user_info.relationship_status),
-         //          age_range: verify.checkUndefined(user_info.age_range),
-         //          birthday: verify.checkUndefined(user_info.birthday),
-         //          about: verify.checkUndefined(user_info.about),
-         //          email: verify.checkUndefined(user_info.email),
-         //          phone: line[1]
-         //       }, function (err, obj) {
-         //          console.log(i);
-         //          asyncInsert(i+1, array);
-         //       });
-         //    }
-         // })
-         // .catch( err => {
-         //    console.log(i + ' - catch error');
-         //    // console.log(err);
-         //    asyncInsert(i+1, array);
-         // })
-      } else {
-         console.log('Finish last line');
-      }
+   if (i < array.length) {
+      var line = array[i].split('|');
+      console.log(line[1]);
+      FacebookUser.create({
+         user_id: line[0],
+         phone: line[1],
+         info: []
+      }, function (err, obj) {
+         console.log('insert ' + i);
+         asyncInsert(i+1, array);
+      });
+      // axios.get('https://graph.facebook.com/' + line[0] +
+      //    '?fields=birthday,age_range,about,address,education,email,first_name,gender,hometown,link,last_name,location,name,middle_name,relationship_status'
+      //    + '&access_token=283111455508809|l0GhSkkg0AHZkDEPimP50eBhz14'
+      // )
+      // .then( data => {
+      //    var user_info = data.data;
+      //    if(user_info.error) {
+      //       console.log(i + '- not exists');
+      //    } else {
+      //       FacebookUser.create({
+      //          user_id: line[0],
+      //          name: verify.checkUndefined(user_info.name),
+      //          first_name : verify.checkUndefined(user_info.first_name),
+      //          last_name : verify.checkUndefined(user_info.last_name),
+      //          middle_name : verify.checkUndefined(user_info.middle_name),
+      //          address: verify.checkUndefined(user_info.address),
+      //          hometown: verify.checkUndefined(user_info.hometown),
+      //          education: verify.checkUndefined(user_info.education),
+      //          gender: verify.checkUndefined(user_info.gender),
+      //          location: verify.checkUndefined(user_info.location),
+      //          relationship_status: verify.checkUndefined(user_info.relationship_status),
+      //          age_range: verify.checkUndefined(user_info.age_range),
+      //          birthday: verify.checkUndefined(user_info.birthday),
+      //          about: verify.checkUndefined(user_info.about),
+      //          email: verify.checkUndefined(user_info.email),
+      //          phone: line[1]
+      //       }, function (err, obj) {
+      //          console.log(i);
+      //          asyncInsert(i+1, array);
+      //       });
+      //    }
+      // })
+      // .catch( err => {
+      //    console.log(i + ' - catch error');
+      //    // console.log(err);
+      //    asyncInsert(i+1, array);
+      // })
+   } else {
+      console.log('Finish last line');
+   }
 }
 
 function asyncReadFile(dirname, i, listFiles) {
@@ -107,6 +142,25 @@ function asyncReadFile(dirname, i, listFiles) {
    }
 }
 
+function updateFbCrontab() {
+   FacebookUser.find({ status: 0 }).limit(599).exec()
+   .then( data => {
+      asyncUpdate(0, data);
+   })
+   .catch( err => {
+      console.log(err);
+      return;
+   })
+}
+
+router.post('/update-fb-user', function (req, res) {
+   var j = schedule.scheduleJob('*/10 * * * *', function(){
+      updateFbCrontab();
+   });
+
+
+})
+
 router.post('/import-fb-user', function (req, res) {
    let dirname = path.join(__root, 'public/test/');
    // let dirname = path.join(__root, 'public/test/');
@@ -115,22 +169,22 @@ router.post('/import-fb-user', function (req, res) {
          // onError(err);
          return err;
       }
-      console.log(filenames);
-      // asyncReadFile(dirname, 0, filenames);
+
+      // if list all file in dirname success
       filenames.forEach(function(filename) {
          fs.readFile(dirname + filename, 'utf-8', function(err, content) {
             if (err) {
                // onError(err);
                return;
             }
+            //if read file success and split content to lines
             content = content.split('\n');
-            console.log(content.length);
+            // console.log(content[0]);
+            //start insert to db
             asyncInsert(0, content);
          });
       });
    });
 });
-
-router.post('/update-empl')
 
 module.exports = router;
